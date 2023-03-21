aece9b4e490abc83c9248edca838bdc2c283074b
Andrew <stariynazik@gmail.com>
2023-03-21 12:57:23 +0300

    index page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
+from data.loginform import LoginForm
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 def main():
@@ -12,50 +16,38 @@ def main():
     app.run()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = []
+    for job in db_sess.query(Jobs).all():
+        elem = job.job, db_sess.query(User).filter(User.id == job.team_leader).first().surname + " " +\
+               db_sess.query(User).filter(User.id == job.team_leader).first().name, job.collaborators,\
+               job.is_finished
+        jobs.append(elem)
+    return render_template('work_list.html', title='Работа', jobs=jobs)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 if __name__ == '__main__':
     main()
-    captain = User()
-    captain.surname = "Scott"
-    captain.name = "Ridley"
-    captain.age = 21
-    captain.position = "captain"
-    captain.speciality = "research engineer"
-    captain.address = "module_1"
-    captain.email = "scott_chief@mars.org"
-    user1 = User()
-    user1.surname = "Donaldson"
-    user1.name = "Jimmy"
-    user1.age = 24
-    user1.position = "PR manager"
-    user1.speciality = "no spec"
-    user1.address = "module_1"
-    user1.email = "mrbeast@mars.org"
-    user2 = User()
-    user2.surname = "Perry"
-    user2.name = "Nicholas"
-    user2.age = 30
-    user2.position = "cook"
-    user2.speciality = "performance arts"
-    user2.address = "module_1"
-    user2.email = "nikocado@mars.org"
-    user3 = User()
-    user3.surname = "McNutt"
-    user3.name = "Ronnie"
-    user3.age = 33
-    user3.position = "guard"
-    user3.speciality = "no spec"
-    user3.address = "module_1"
-    user3.email = "ronnut@mars.org"
-    job1 = Jobs()
-    job1.team_leader = 1
-    job1.job = "deployment of residential modules 1 and 2"
-    job1.work_size = 15
-    job1.collaborators = "2, 3"
-    job1.is_finished = False
-    db_sess = db_session.create_session()
-    db_sess.add(captain)
-    db_sess.add(user1)
-    db_sess.add(user2)
-    db_sess.add(user3)
-    db_sess.add(job1)
-    db_sess.commit()
